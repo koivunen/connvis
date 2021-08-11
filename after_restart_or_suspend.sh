9329f7a04bd02a48d1cd09af767b7674bebe6a81
lamkoi <lamkoi@utu.fi>
2021-08-11 16:10:00 +0300

    cleanup

---
+++ after_restart_or_suspend.sh
@@ -0,0 +1,2 @@
+systemctl restart hostapd
+echo 1 > /proc/sys/net/netfilter/nf_conntrack_acct
--- install_massdns.sh
+++ install_massdns.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/blechschmidt/massdns.git
 cd massdns
 make
-
+make install
