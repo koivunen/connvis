26ea94a06d833e641fa320552d0b6d2ca0300013
lamkoi <lamkoi@utu.fi>
2021-08-13 21:29:15 +0300

    Parse args

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+import argparse
+parser = argparse.ArgumentParser(prog="connvis",description='Web-based conntrack that tries to simplify the data for privacy research')
+parser.add_argument('--nodnsseed', help='do not seed domains from dnsmasq history',action='store_true')
+parser.add_argument('--shell', help='Enable interactive shell',action='store_true')
+args = parser.parse_args()
+
+
 import ipaddress
 
 homenetwork = ipaddress.ip_network('192.168.0.0/24')
